551813df7bb2286ac09fe085a5f02c9f39f27090
Clay Diffrient <clay.diffrient@gmail.com>
2019-07-31 23:53:39 -0600

    Add homebrew stuff

---
+++ homebrew/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This is slightly modified from https://github.com/pbouzakis/dotfiles/blob/master/homebrew/install.sh
+
+# Installs Homebrew and some of the common dependencies needed/desired for software development
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade --all
+
+brew tap homebrew/bundle
+
+brew bundle --file=homebrew/Brewfile
+
+brew install yarn
