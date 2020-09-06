64d592aef208b717cd2939685376fa5142954e91
Clay Diffrient <clay.diffrient@gmail.com>
2020-09-06 04:42:12 +0000

    Use newer vim on linux

--- apt/install.sh
+++ apt/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:jonathonf/vim
 sudo apt-get update
 PACKAGEPATH="$(dirname "$(readlink -f "$0")")/packages.txt"
 cat $PACKAGEPATH | xargs sudo apt-get -y install
